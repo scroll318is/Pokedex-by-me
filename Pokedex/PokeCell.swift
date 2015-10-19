106c07233bcc1db3afa97a2edaa877ba90bed7dd
Stoyan Kostov <stoyanykostov@gmail.com>
2015-10-19 15:47:36 +0300

    Added custom collection view cell

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Stoyan Yordanov Kostov on 10/19/15.
+//  Copyright © 2015 com.kostov. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon)
+    {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
